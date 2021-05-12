9fb405f68c39b55b25ba475f4087ccdd04ec9634
belmir <belmirmohammed7@gmail.com>
2021-05-13 00:44:29 +0200

    [init][add] prisma

---
+++ prisma/prisma.controller.ts
@@ -0,0 +1,19 @@
+import { PrismaClient, UserRole } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export async function inserUser(req: Request, res:Response) {
+    
+
+    // try {
+
+    //     const insert = await  prisma.user.create({
+    //         data:{
+    //             name:
+    //         }
+    //     })
+        
+    // } catch (error) {
+        
+    // }
+    
+}
